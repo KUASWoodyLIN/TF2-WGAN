f4d99ba27d8c40f86c8710ff639b6431a69e7f33
KUASWoodyLin <jj7158761@gmail.com>
2019-08-20 16:28:16 -0400

    fix preprocessing function

--- utils/dataset.py
+++ utils/dataset.py
@@ -7,6 +7,5 @@ def parse_fn(dataset, input_size=(64, 64)):
     h, w, _ = x.shape
     x = tf.image.crop_to_bounding_box(x, (h-crop_size)//2, (w-crop_size)//2, crop_size, crop_size)
     x = tf.image.resize(x, input_size)
-    x = tf.clip_by_value(x, 0, 255)
     x = x / 127.5 - 1
     return x
